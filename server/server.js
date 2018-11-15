7849589be3f54a8817966d7323b4ea05ba35a329
ben-vandelaar <benvandelaar@gmail.com>
2018-11-15 15:46:08 +1300

    getting ready for deploy

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const path = require('path')
 const posts = require('../routes/api/posts');
 
 const app = express();
@@ -20,6 +20,15 @@ mongoose
 
 //Use Routes
 app.use('/api/posts', posts)
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const port = process.env.PORT || 5000
 
